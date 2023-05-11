d65676de35bf6efc9a2e3847c37dfc4062540480
LisyOdli <lisy.odli2@gmail.com>
2023-05-11 00:52:03 -0400

    Agregado un + a diseño

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return HttpResponse("Hola Mundo :D creamos nuestra primera vista. Y esta prueba es para saber si funciona el git")
+    return HttpResponse("Hola Mundo :D creamos nuestra primera vista. Y esta prueba es para saber si funciona el git+")
 
 def autenticar(request):
     return render(request, "autentication.html")
